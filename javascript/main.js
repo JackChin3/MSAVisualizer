275d7f4ada849094e4c3584c3c8b54ff5263504c
Jack Chin <jack@Jacks-MacBook-Air-5.local>
2023-11-29 16:15:14 -0800

    Fixed Electron launching with new main.js file

---
+++ javascript/main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('examples/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+})
+
+console.log('Hello from Electron 👋')
